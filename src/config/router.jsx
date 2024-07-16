d4a3c16f606fce16401bd77fb3bdd05b0bb12e66
mubbashirhussain <mubbashirhhussain@gmail.com>
2024-07-16 15:41:56 +0500

    Create Room/call page UI

--- src/config/router.jsx
+++ src/config/router.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/home";
+import Room from "../pages/room";
 
 function Approuter() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/room/:id" element={<Room />} />
       </Routes>
     </>
   );
