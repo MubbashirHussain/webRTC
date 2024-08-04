932de3ed63621302fd00e1d934d639c37be0596b
mubbashirhussain <mubbashirhhussain@gmail.com>
2024-08-04 16:44:24 +0500

    Install Nodemon and write Scripts for runing Server

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = requrie('express')
 const app = express();
 
 
-app.get('/', (req, res) => { });
+app.get('/', (req, res) => { res.send("API is working") });
 
 
 app.listen(process.env.PORT || 8000, () => console.log("server is Runing at PORT " + process.env.PORT))
